eb78b354f77232a38cff2f2fd7ec4426d06bce43
Yaroslav Dyshkantiuk <yaroslav.dyshkantiuk@gmail.com>
2022-02-14 13:04:24 +0200

    useRef

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useState } from 'react';
+import ClassCounter from './components/ClassCounter';
+import Counter from './components/Counter';
+import Postitem from './components/PostItem';
+import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
+import './styles/App.css';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: 'Javascript',
+      body: 'Description',
+    },
+    {
+      id: 2,
+      title: 'Javascript 2',
+      body: 'Description',
+    },
+    {
+      id: 3,
+      title: 'Javascript 3',
+      body: 'Description',
+    },
+  ]);
+
+  const [post, setPost] = useState({ title: '', body: '' });
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+    setPosts([...posts, { ...post, id: Date.now() }]);
+    setPost({ title: '', body: '' });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form>
+        {/* Управляемый компонет */}
+        <MyInput
+          value={post.title}
+          onChange={(e) => setPost({ ...post, title: e.target.value })}
+          type='text'
+          placeholder={'Post title'}
+        />
+        {/* Неуправляемый компонент */}
+        <MyInput
+          value={post.body}
+          type='text'
+          onChange={(e) => setPost({ ...post, body: e.target.value })}
+          placeholder={'Post description'}
+        />
+        <MyButton onClick={addNewPost}>Add post</MyButton>
+      </form>
+      <PostList posts={posts} title='Post about JS' />
     </div>
   );
 }

129ab8d8bf26993d90c566919c814e64cad843d2
Yaroslav Dyshkantiuk <yaroslav.dyshkantiuk@gmail.com>
2022-02-17 14:10:02 +0200

    Modal window

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useMemo, useState } from 'react';
 import PostFilter from './components/PostFilter';
 import PostForm from './components/PostForm';
 import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyModal from './components/UI/MyModal/MyModal';
 import './styles/App.css';
 
 function App() {
@@ -24,6 +26,7 @@ function App() {
   ]);
 
   const [filter, setFilter] = useState({ sort: '', query: '' });
+  const [modal, setModal] = useState(false);
 
   const sortedPost = useMemo(() => {
     if (filter.sort) {
@@ -42,6 +45,7 @@ function App() {
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
+    setModal(false);
   };
 
   const removePost = (post) => {
@@ -50,7 +54,12 @@ function App() {
 
   return (
     <div className='App'>
-      <PostForm create={createPost} />
+      <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
+        Create user
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />
+      </MyModal>
       <hr style={{ margin: '15px 0' }} />
       <PostFilter filter={filter} setFilter={setFilter} />
       <PostList

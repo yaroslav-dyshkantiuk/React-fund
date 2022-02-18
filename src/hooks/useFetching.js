ccba023242f5b4d57b6da58d531abbc56cc4ce92
Yaroslav Dyshkantiuk <yaroslav.dyshkantiuk@gmail.com>
2022-02-18 13:09:54 +0200

    useFetching

---
+++ src/hooks/useFetching.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+export const useFetching = (callback) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+  const fetching = async (callback) => {
+    try {
+      setIsLoading(true);
+      await callback();
+    } catch (e) {
+      setError(e.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return [fetching, isLoading, error];
+};

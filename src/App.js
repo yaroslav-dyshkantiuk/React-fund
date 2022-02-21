599678e62f9cfee9683878231c8792cf931142b2
Yaroslav Dyshkantiuk <yaroslav.dyshkantiuk@gmail.com>
2022-02-21 18:14:52 +0200

    Page navigation

--- src/App.js
+++ src/App.js
@@ -1,80 +1,14 @@
-import { useEffect, useState } from 'react';
-import PostFilter from './components/PostFilter';
-import PostForm from './components/PostForm';
-import PostList from './components/PostList';
-import MyButton from './components/UI/button/MyButton';
-import MyModal from './components/UI/MyModal/MyModal';
-import { usePosts } from './hooks/usePosts';
+import { BrowserRouter } from 'react-router-dom';
+import AppRouter from './components/UI/AppRouter';
+import Navbar from './components/UI/Navbar/Navbar';
 import './styles/App.css';
-import PostService from './API/PostService';
-import Loader from './components/UI/Loader/Loader';
-import { useFetching } from './hooks/useFetching';
-import { getPageCount, getPagesArray } from './utils/page';
-import Pagination from './components/UI/pagination/Pagination';
 
 function App() {
-  const [posts, setPosts] = useState([]);
-
-  const [filter, setFilter] = useState({ sort: '', query: '' });
-  const [modal, setModal] = useState(false);
-  const [totalPages, setTotalPage] = useState(0);
-  const [limit, setLimit] = useState(10);
-  const [page, setPage] = useState(1);
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-
-  const [fetchPosts, isPostsLoading, postError] = useFetching(
-    async (limit, page) => {
-      const response = await PostService.getAll(limit, page);
-      setPosts(response.data);
-      const totalCount = response.headers['x-total-count'];
-      setTotalPage(getPageCount(totalCount, limit));
-    }
-  );
-
-  useEffect(() => {
-    fetchPosts(limit, page);
-  }, []);
-
-  const createPost = (newPost) => {
-    setPosts([...posts, newPost]);
-    setModal(false);
-  };
-
-  const removePost = (post) => {
-    setPosts(posts.filter((p) => p.id !== post.id));
-  };
-
-  const changePage = (page) => {
-    setPage(page);
-    fetchPosts(limit, page);
-  };
-
   return (
-    <div className='App'>
-      <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
-        Create user
-      </MyButton>
-      <MyModal visible={modal} setVisible={setModal}>
-        <PostForm create={createPost} />
-      </MyModal>
-      <hr style={{ margin: '15px 0' }} />
-      <PostFilter filter={filter} setFilter={setFilter} />
-      {postError && <h1>An error occurred ${postError}</h1>}
-      {isPostsLoading ? (
-        <div
-          style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}
-        >
-          <Loader />
-        </div>
-      ) : (
-        <PostList
-          remove={removePost}
-          posts={sortedAndSearchedPosts}
-          title='Post about JS'
-        />
-      )}
-      <Pagination page={page} changePage={changePage} totalPages={totalPages} />
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <AppRouter />
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Error.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+  return (
+    <div>
+      <h1 style={{ color: 'red' }}>You have landed on a non-existent page</h1>
+    </div>
+  );
+};
+
+export default Error;
